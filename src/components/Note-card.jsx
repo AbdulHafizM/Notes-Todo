9c5c1941e74d61f32ba01101f9d92ad2044ef71a
AbdulHafizM <muibiabdulhafiz007@gmail.com>
2023-11-07 15:06:38 +0100

    update

--- src/components/Note-card.jsx
+++ src/components/Note-card.jsx
@@ -6,9 +6,9 @@ const NoteCard = ({item}) => {
     const navigate = useNavigate()
     return(
         <button className="note-card" onClick={() => navigate(`/note/${item._id}`)}>
-            <div className="title">{
-                item.title == '' ? item.body.length > 15 ? item.body.slice(0,15) + '...' : item.body : item.title.length > 15 ? item.title.slice(0,15) : item.title
-            }</div>
+            <div className="title">
+                {item.title == '' ? item.body.length > 15 ? item.body.slice(0,15) + '...' : item.body : item.title.length > 15 ? item.title.slice(0,15) : item.title}
+            </div>
             <div><span>{timeFormatter(item.createdAt)},</span><span>{dateFormatter(item.createdAt)}</span></div>
         </button>
     )
